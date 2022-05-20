98d80182bb68407a489a17a151c455d759378632
global <asgagagsad@gmail.com>
2022-05-20 11:10:40 -0400

    test1

--- square.go
+++ square.go
@@ -9,14 +9,19 @@ type Square struct {
 	a     uint
 }
 
-func (receiver) End() Point {
-	// implement me
+func (s *Square) End() Point {
+	var b = Point{
+		x: s.start.x + int(s.a),
+		y: s.start.y + int(s.a),
+	}
+	return b
+
 }
 
-func (receiver) Area() uint {
-	// implement me
+func (s *Square) Area() uint {
+	return s.a * s.a
 }
 
-func (receiver) Perimeter() uint {
-	// implement me
+func (s *Square) Perimeter() uint {
+	return s.a * 4
 }
